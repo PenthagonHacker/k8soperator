ed85f1d36038c8f00299215f6af92d841b8aa8d2
PenthagonHacker <70442718+PenthagonHacker@users.noreply.github.com>
2026-05-02 00:04:09 +0300

    Add files via upload

---
+++ dags/k8s_operator_dag.py
@@ -0,0 +1,20 @@
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from datetime import datetime
+
+with DAG(
+    dag_id="k8s_python_job",
+    start_date=datetime(2024, 1, 1),
+    schedule=None,
+    catchup=False,
+) as dag:
+
+    run_script = KubernetesPodOperator(
+        task_id="run_python_script",
+        name="run-python-script",
+        namespace="airflow",
+        image="my-python-job:latest",
+        cmds=["python", "script.py"],
+        get_logs=True,
+        is_delete_operator_pod=True,
+    )
