99154e94466915b64206570ad92bb9180094f1fa
PenthagonHacker <70442718+PenthagonHacker@users.noreply.github.com>
2026-05-02 19:15:57 +0300

    Create k8s_operator_test.py

---
+++ k8s_operator_test.py
@@ -0,0 +1,25 @@
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+from datetime import datetime
+
+with DAG(
+    dag_id="k8s_operator_test",
+    start_date=datetime(2024, 1, 1),
+    schedule=None,
+    catchup=False,
+) as dag:
+
+    test_k8s = KubernetesPodOperator(
+        task_id="run_test_pod",
+        name="k8s-test-pod",
+        namespace="airflow",
+
+        # use public image (no registry headaches)
+        image="python:3.11-slim",
+
+        cmds=["python", "-c"],
+        arguments=["import time; print('start'); time.sleep(30); print('end')"],
+
+        get_logs=True,
+        is_delete_operator_pod=True,
+    )
