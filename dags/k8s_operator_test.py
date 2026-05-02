da62c80dcabb949f8463833c422607088093b3e8
PenthagonHacker <70442718+PenthagonHacker@users.noreply.github.com>
2026-05-02 19:16:24 +0300

    Update and rename k8s_operator_test.py to dags/k8s_operator_test.py

--- dags/k8s_operator_test.py
+++ dags/k8s_operator_test.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 with DAG(
     dag_id="k8s_operator_test",
-    start_date=datetime(2024, 1, 1),
+    start_date=datetime(2026, 5, 1),
     schedule=None,
     catchup=False,
 ) as dag:
