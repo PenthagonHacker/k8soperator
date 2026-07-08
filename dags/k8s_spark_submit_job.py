1616d3453a33da14a47f0642dd2ebf8ab93aa627
PenthagonHacker <70442718+PenthagonHacker@users.noreply.github.com>
2026-07-08 19:11:22 +0300

    Update k8s_spark_submit_job.py

--- dags/k8s_spark_submit_job.py
+++ dags/k8s_spark_submit_job.py
@@ -18,7 +18,7 @@ with DAG(
         name="spark-submit-demo",
         namespace="airflow",
 
-        image="rtlabsipr/pyspark-submit-demo:1.0",
+        image="rtlabsipr/pyspark-submit-demo:2.0",
 
         cmds=[
             "spark-submit"
@@ -50,7 +50,7 @@ with DAG(
             "spark.kubernetes.namespace=airflow",
 
             "--conf",
-            "spark.kubernetes.container.image=rtlabsipr/pyspark-submit-demo:1.0",
+            "spark.kubernetes.container.image=rtlabsipr/pyspark-submit-demo:2.0",
 
             "--conf",
             "spark.kubernetes.authenticate.driver.serviceAccountName=spark",
